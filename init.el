ee6709151b94376d340b896be3c9447eee40702a
Jean-Marc Saffroy <saffroy@gmail.com>
2020-05-03 19:57:58 +0200

    do not use iceweasel as browser

--- init.el
+++ init.el
@@ -26,6 +26,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(browse-url-firefox-program "firefox")
  '(c-default-style "linux")
  '(case-fold-search t)
  '(column-number-mode t)
