ecac626a3edd59fc72b8d64ea4ebd9e0925308e6
Jean-Marc Saffroy <saffroy@gmail.com>
2021-04-09 19:18:47 +0200

    tune GC to improve startup time

--- init.el
+++ init.el
@@ -3,6 +3,19 @@
 ;; without this, setting the font lags for a few seconds
 ;; (modify-frame-parameters nil '((wait-for-wm . nil)))
 
+
+;; The default is 800 kilobytes.  Measured in bytes.
+(setq gc-cons-threshold (* 100 1000 1000))
+
+;; Profile emacs startup
+;; (add-hook 'emacs-startup-hook
+;;           (lambda ()
+;;             (message "*** Emacs loaded in %s with %d garbage collections."
+;;                      (format "%.2f seconds"
+;;                              (float-time
+;;                               (time-subtract after-init-time before-init-time)))
+;;                      gcs-done)))
+
 ;; **exit**
 ;; https://emacs.stackexchange.com/a/28927
 (defun my-exit ()
